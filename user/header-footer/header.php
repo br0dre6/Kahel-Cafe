d18ca69eb4ac194f013bd499d53db569d793c26f
br0dre6 <solisdrei06@gmail.com>
2024-09-15 15:36:13 +0800

    landing page

---
+++ user/header-footer/header.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="styles.css">
+    <title>Kahel Cafe</title>
+</head>
+<body>
+    <header>
+        <div class="container">
+            <img class="brand-img" src="icons/kahel-cafe-logo.png" width="48px"/>
+    
+            <a class="link" href="">Home</a>
+            <a class="link" href="order-now.php">Order Now</a>
+            <a class="link" href="reservation.php">Reservation</a>
+            <img class="notification-bell" src="icons/bell-solid.svg" width="15px"/>
+            <img class="cart" src="icons/shopping-cart.png" width="15px"/>
+            <a class="link" href="login.php">Login</a>
+        </div>
+    </header>
---
+++ user/index.php
@@ -0,0 +1,15 @@
+<?php include 'header-footer/header.php'; ?>
+
+<div class="kahel-cafe-banner">
+    <img class="banner" src="kahel-cafe-banner.jpg"/>
+</div>
+        
+<div class="special-offers">
+    <div class="special-offers-container">
+
+    </div>
+
+    <div class="special-offers-1">
+
+    </div>
+</div>
